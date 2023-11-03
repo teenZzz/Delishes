11d8d83015e4aac009b201ce825ddf547980ec89
user1 <user1@DESKTOP-SO45SOP>
2023-11-03 12:20:09 +0300

    Обновлена стартовая активность

--- app/src/main/java/com/example/delishes/MainScreen.java
+++ app/src/main/java/com/example/delishes/MainScreen.java
@@ -2,6 +2,9 @@ package com.example.delishes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,6 +15,7 @@ import com.example.delishes.databinding.ActivitySplashScreenBinding;
 public class MainScreen extends AppCompatActivity {
 
     private ActivityMainScreenBinding binding;
+    private static final String MY_SETTINGS = "my_settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,16 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         binding = ActivityMainScreenBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+
+        SharedPreferences sp = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE);
+        boolean hasVisited = sp.getBoolean("hasVisited", false);
+
+        if(!hasVisited){
+            Intent intent = new Intent(this, StartActivity.class);
+            startActivity(intent);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("hasVisited", true);
+            editor.apply();
+        }
     }
 }
--- app/src/main/java/com/example/delishes/StartActivity.java
+++ app/src/main/java/com/example/delishes/StartActivity.java
@@ -2,13 +2,35 @@ package com.example.delishes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.delishes.databinding.ActivityMainScreenBinding;
+import com.example.delishes.databinding.ActivityStartBinding;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ActivityStartBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binding = ActivityStartBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Intent intent = new Intent(this, MainScreen.class);
+        binding.filledButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
