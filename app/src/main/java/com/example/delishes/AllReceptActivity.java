772b9b03a715191cf86ad2d4003eea880ec82911
Artem <bartem2004@mail.ru>
2023-11-07 17:03:16 +0300

    Доделан MainScreen и созданы новые активности

---
+++ app/src/main/java/com/example/delishes/AllReceptActivity.java
@@ -0,0 +1,14 @@
+package com.example.delishes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllReceptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_recept);
+    }
+}
